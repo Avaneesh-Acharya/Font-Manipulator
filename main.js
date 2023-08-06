de76506d3fee984a83f38da8ffd30aa68622dfe7
Avaneesh-Acharya <137638840+Avaneesh-Acharya@users.noreply.github.com>
2023-08-06 11:36:56 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,19 @@
 function setup() {
-    canvas=createCanvas(450, 450)
-
+    canvas = createCanvas(630, 790)
+    canvas.position(770, 100)
+    video = createCapture(VIDEO)
+    video.size(450, 450)
+    PoseNet=ml5.poseNet(video, model_loaded())
+    PoseNet.on('pose', got_poses)
+}
+function model_loaded() {
+    console.log("Model is initialized")
+}
+function got_poses(results) {
+    if (results.length>0) {
+        console.log(results)
+    }
+}
+function draw() {
+    background("#999999")
 }
