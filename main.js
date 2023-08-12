ca47022600876246da5d03d1f83b1674529df196
Avaneesh-Acharya <137638840+Avaneesh-Acharya@users.noreply.github.com>
2023-08-12 11:40:11 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,19 +1,35 @@
+noseX = 0
+noseY = 0
+difference = 0
+rightWristX = 0
+leftWristX = 0
 function setup() {
     canvas = createCanvas(630, 790)
     canvas.position(770, 100)
     video = createCapture(VIDEO)
-    video.size(450, 450)
-    PoseNet=ml5.poseNet(video, model_loaded())
+    video.size(750, 450)
+    video.position(50, 200)
+    PoseNet = ml5.poseNet(video, model_loaded())
     PoseNet.on('pose', got_poses)
 }
 function model_loaded() {
     console.log("Model is initialized")
 }
 function got_poses(results) {
-    if (results.length>0) {
+    if (results.length > 0) {
         console.log(results)
+        noseX = results[0].pose.nose.x
+        noseY = results[0].pose.nose.y
+        rightWristX = results[0].pose.rightWrist.x
+        leftWristX = results[0].pose.leftWrist.x
+        difference = floor(leftWristX - rightWristX)
     }
 }
 function draw() {
     background("#999999")
+    fill("#2A93C3")
+    stroke("#010101")
+    textSize(difference-50)
+    text( "Avaneesh", noseX, noseY)
+    document.getElementById("square_sides").innerHTML = "Width and Height of text will be " + difference + "px"
 }
